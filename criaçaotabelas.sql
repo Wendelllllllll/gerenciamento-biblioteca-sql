6104f080a0f86d7e2ab704e4c7376a80d43f8336
henriquecorrea-fiec <henrique38292@fiec.edu.br>
2025-03-28 20:27:01 -0300

    att da burrice do henrique

--- criaçaotabelas.sql
+++ criaçaotabelas.sql
@@ -1,8 +1,17 @@
+CREATE TABLE autores (
+    id_autor INT PRIMARY KEY IDENTITY(1,1),
+    nome VARCHAR(100),
+    sobrenome VARCHAR(50),
+    data_nascimento DATE,
+    nacionalidade VARCHAR(50)
+);
+
 create table categoria (
   id_categoria int primary key identity(1,1),
   nome varchar(50) not null,
   descricao text
 );
+
  create table livros (
     id_livro int not null primary key identity(1,1);
     titulo varchar(100),
@@ -10,7 +19,13 @@ create table categoria (
     id_autor int,
     id_categoria int,
     ano_publicacao int(10)
+);
 
-
-)
+CREATE TABLE emprestimos (
+    id_emprestimo INT PRIMARY KEY IDENTITY(1,1),
+    id_livro INT,
+    data_emprestimo DATE,
+    data_devolucao DATE,
+    status VARCHAR(50)
+);
 
